2ff46fb66b28395ada31bda5b8702a0597afcd79
Kaibanchu <Haoquatest2@gmail>
2024-01-28 01:34:09 +0700

    fix UI delete submit

--- src/component/Game.jsx
+++ src/component/Game.jsx
@@ -26,7 +26,6 @@ const Game = () => {
   const [checkAnswer, setCheckAnswer] = useState("");
   const [add, setAdd] = useState(false);
 
-
   const handleSetTypes = (symbol) => {
     const indexTypes = groupTypes.findIndex((t) => t === symbol);
     let prevTypes = [...groupTypes];
@@ -50,7 +49,6 @@ const Game = () => {
     const types = groupTypes[typesIndex];
     const num1 = randomNumber(Number(mins), Number(maxs));
     const num2 = randomNumber(Number(mins), Number(maxs));
-    
 
     if (types === "+") {
       setQuestion(`${num1} ${types} ${num2}`);
@@ -83,10 +81,10 @@ const Game = () => {
       const answerIndex = Math.floor(Math.random() * 4);
       answerCheck[answerIndex] = answer;
       if (answerIndex !== 0) {
-        answerCheck[0] = answer - 3;
+        answerCheck[0] = answer - 1;
       }
       if (answerIndex !== 1) {
-        answerCheck[1] = answer + 3;
+        answerCheck[1] = answer + 1;
       }
       if (answerIndex !== 2) {
         answerCheck[2] = answer + 2;
@@ -108,21 +106,21 @@ const Game = () => {
       }, 2000);
     }
   };
-  const handleSubmit = () => {
+  const handleSubmit = (number) => {
     if (count > 0) {
       //console.log(parseInt(result), parseInt(answer));
-      if (checkAnswer == answer) {
+      if (number == answer) {
         new Audio("src/component/soundCorrect.mp3").play();
         setCount(timeInput); // gia lập giá trị nhập vao la 15
         setScore(score + 1);
         setAdd(true);
         setOptions([]);
         setTypes("");
-       
-      setTimeout(() => {
+
+        setTimeout(() => {
           generateQuestion();
           setSubmit(true);
-        }, 300); 
+        }, 300);
       } else {
         new Audio("src/component/soundError.mp3").play();
         setTimeout(() => {
@@ -217,7 +215,6 @@ const Game = () => {
     }
   }, [startGame, count]);
 
-
   return (
     <>
       {confetti && (
@@ -265,10 +262,9 @@ const Game = () => {
               {symbols.map((symbol) => (
                 <button
                   key={symbol}
-                 
                   style={
                     groupTypes.includes(symbol, 0)
-                      ? { backgroundColor: "orange" }
+                      ? { backgroundColor: "rgb(240, 212, 165)" }
                       : {}
                   }
                   onClick={() => handleSetTypes(symbol)}
@@ -285,51 +281,91 @@ const Game = () => {
         ) : count > 0 && wrong === 0 ? (
           <div className="calculator">
             <div className="header">
-              <div className="reset" onClick={handleReset}>
-              
-                <button>Reset
-                <svg xmlns="http://www.w3.org/2000/svg" width="12" height="12" fill="currentColor" viewBox="0 0 16 16">
-                                    <path fillRule="evenodd" d="M8 3a5 5 0 1 1-4.546 2.914.5.5 0 0 0-.908-.417A6 6 0 1 0 8 2v1z"/>
-                                    <path d="M8 4.466V.534a.25.25 0 0 0-.41-.192L5.23 2.308a.25.25 0 0 0 0 .384l2.36 1.966A.25.25 0 0 0 8 4.466z"/>
-                                </svg>
-                </button>
+              <div className="count">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="20"
+                  height="20"
+                  fill="currentColor"
+                  className="bi bi-stopwatch"
+                  viewBox="1 -2.2 18 18"
+                >
+                  <path d="M8.5 5.6a.5.5 0 1 0-1 0v2.9h-3a.5.5 0 0 0 0 1H8a.5.5 0 0 0 .5-.5z" />
+                  <path d="M6.5 1A.5.5 0 0 1 7 .5h2a.5.5 0 0 1 0 1v.57c1.36.196 2.594.78 3.584 1.64a.715.715 0 0 1 .012-.013l.354-.354-.354-.353a.5.5 0 0 1 .707-.708l1.414 1.415a.5.5 0 1 1-.707.707l-.353-.354-.354.354a.512.512 0 0 1-.013.012A7 7 0 1 1 7 2.071V1.5a.5.5 0 0 1-.5-.5M8 3a6 6 0 1 0 .001 12A6 6 0 0 0 8 3" />
+                </svg>
+                {count}
               </div>
               <div className="score">
                 <strong>{score}</strong>
               </div>
-              <div className="count">
-              <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" className="bi bi-stopwatch" viewBox="1 -2.2 18 18">
-                    <path d="M8.5 5.6a.5.5 0 1 0-1 0v2.9h-3a.5.5 0 0 0 0 1H8a.5.5 0 0 0 .5-.5z"/>
-                    <path d="M6.5 1A.5.5 0 0 1 7 .5h2a.5.5 0 0 1 0 1v.57c1.36.196 2.594.78 3.584 1.64a.715.715 0 0 1 .012-.013l.354-.354-.354-.353a.5.5 0 0 1 .707-.708l1.414 1.415a.5.5 0 1 1-.707.707l-.353-.354-.354.354a.512.512 0 0 1-.013.012A7 7 0 1 1 7 2.071V1.5a.5.5 0 0 1-.5-.5M8 3a6 6 0 1 0 .001 12A6 6 0 0 0 8 3"/>
-                    </svg>
-               <strong>{count}</strong>
+
+              <div className="reset" onClick={handleReset}>
+                <button>
+                  Reset
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="12"
+                    height="12"
+                    fill="currentColor"
+                    viewBox="0 0 16 16"
+                  >
+                    <path
+                      fillRule="evenodd"
+                      d="M8 3a5 5 0 1 1-4.546 2.914.5.5 0 0 0-.908-.417A6 6 0 1 0 8 2v1z"
+                    />
+                    <path d="M8 4.466V.534a.25.25 0 0 0-.41-.192L5.23 2.308a.25.25 0 0 0 0 .384l2.36 1.966A.25.25 0 0 0 8 4.466z" />
+                  </svg>
+                </button>
               </div>
             </div>
 
             <div className="game">{question} = ?</div>
             <div className="number">
-              {options.map((number) => (
-                <button
-                  key={number}
-                  className={options.indexOf(number)}
-                  onClick={() => {
-                    handleCheckAnwer(number), setTypes(number);
-                  }}
-                style={types === number ? { backgroundColor: " rgb(212, 77, 212)" } : {}}
-                >
-                  {number} 
-                  
-                </button>
-              ))}
-            </div>
+              <button
+                className="n0"
+                key={options[0]}
+                onClick={() => {
+                  handleCheckAnwer(options[0]),
+                    setTypes(options[0]),
+                    handleSubmit(options[0]);
+                }}
+              >
+                {options[0]}
+              </button>
+              <button
+                className="n1"
+                key={options[1]}
+                onClick={() => {
+                  handleCheckAnwer(options[1]),
+                    setTypes(options[1]),
+                    handleSubmit(options[1]);
+                }}
+              >
+                {options[1]}
+              </button>
 
-            <div className="submit" onClick={handleSubmit}>
-              Submit
-
-              <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="currentColor" viewBox="0 0 16 16">
-                                    <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"/>
-                                    <path d="M10.97 4.97a.235.235 0 0 0-.02.022L7.477 9.417 5.384 7.323a.75.75 0 0 0-1.06 1.06L6.97 11.03a.75.75 0 0 0 1.079-.02l3.992-4.99a.75.75 0 0 0-1.071-1.05z"/>
-                                </svg>
+              <button
+                className="n2"
+                key={options[2]}
+                onClick={() => {
+                  handleCheckAnwer(options[2]),
+                    setTypes(options[2]),
+                    handleSubmit(options[2]);
+                }}
+              >
+                {options[2]}
+              </button>
+              <button
+                className="n3"
+                key={options[3]}
+                onClick={() => {
+                  handleCheckAnwer(options[3]),
+                    setTypes(options[3]),
+                    handleSubmit(options[3]);
+                }}
+              >
+                {options[3]}
+              </button>
             </div>
           </div>
         ) : (
